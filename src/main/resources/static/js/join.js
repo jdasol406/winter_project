bf2652751208f19e983879c325457c7ff9765599
jdasol406 <jdasol406@gmail.com>
2023-11-17 01:01:04 +0900

    20231117 Mid-commit

--- src/main/resources/static/js/join.js
+++ src/main/resources/static/js/join.js
@@ -1,3 +1,20 @@
-/**
- * 
- */
+/* 비밀번호, 닉네임 유효성 체크 */
+function pwCheck() {
+  var pw = $("#password").val();
+  var number = pw.search(/[0-9]/g);
+  var english = pw.search(/[a-z]/ig);
+  
+  if (pw.length < 8 || number < 0 || english < 0) {
+    $('#pw-hidden').css('display','block');
+    $('password').css('border','1px, solid, red');
+    return false;
+  } 
+  
+  if (false) {
+    return false;
+  } else {
+    return true;
+  }
+  
+  
+}
