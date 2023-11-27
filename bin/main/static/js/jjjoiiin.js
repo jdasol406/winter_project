8aefc856a48d08420850fd73e5d5fb7e5f35cce2
jdasol406 <jdasol406@gmail.com>
2023-11-27 23:59:19 +0900

    20231127

---
+++ bin/main/static/js/jjjoiiin.js
@@ -0,0 +1,161 @@
+/* 필수항목 체크 함수 */
+function emailNecessaryCheck() {
+  if($('#email').val() === ''){
+    $('#email-hidden2').css('display','block');
+    $('#email').css('outline','0.5px solid red');
+    $('#email-title').css('color','#f67877');
+    return false;
+  } else {
+    $('#email-hidden2').css('display','none');
+    $('#email').css('outline','none');
+    $('#email-title').css('color','#000');
+    $('#email-btn').css('pointer-events','auto');
+    return true;
+  }
+}
+
+/* email-btn 클릭 함수 */
+function emailButtonClick() {
+  console.log('click');
+}
+
+$('#email-btn').click(emailButtonClick);
+
+function pwCheckNecessaryCheck() {
+  if($('#password-check').val() === ''){
+    $('#pwCheck-hidden2').css('display','block');
+    $('#password-check').css('outline','0.5px solid red');
+    return false;
+  } else {
+    $('#pwCheck-hidden2').css('display','none');
+    return true;
+  }
+}
+
+function nicknameCheck() {
+  if($('#nickname').val() === ''){
+    $('#nickname-hidden2').css('display','block');
+    $('#nickname').css('outline','0.5px solid red');
+    $('#nickname-title').css('color','#f67877');
+    return false;
+  } else {
+    $('#nickname-hidden2').css('display','none');
+    $('#nickname').css('outline','none');
+    $('#nickname-title').css('color','#000');
+    return true;
+  }
+}
+
+//------------------------------------------------------------------------
+
+/* 유효성 체크 */
+function pwCheck() {
+  var pw = $("#password").val();
+  var number = pw.search(/[0-9]/g);
+  var english = pw.search(/[a-z]/ig);
+  
+  if (pw.length < 8 || number < 0 || english < 0) {
+    $('#pw-hidden').css('display','block');
+    $('#password').css('outline','0.5px solid red');
+    $('#pw-title').css('color','#f67877');
+  } else {
+    $('#pw-hidden').css('display','none');
+    $('#password').css('outline','none');
+    $('#pw-title').css('color','#000');
+    return true;
+  }
+}
+
+function pwNecessaryCheck() {
+  if($('#password').val() === ''){
+    $('#pw-hidden2').css('display','block');
+    $('#password').css('outline','0.5px solid red');
+  }  else {
+    $('#pw-hidden2').css('display','none');
+    return true;
+  }
+}
+
+function pwDoubleCheck() {
+  if ($('#password').val() !== $('#password-check').val()) {
+    $('#pwCheck-hidden').css('display', 'block');
+    $('#password-check').css('outline','0.5px solid red');
+    $('#pw-check-title').css('color','#f67877');
+  } else {
+    $('#pwCheck-hidden').css('display', 'none');
+    $('#password-check').css('outline','none');
+    $('#pw-check-title').css('color','#000');
+    return true;
+  }
+}
+
+//------------------------------------------------------------------------
+
+/* checkbox */
+// #all-check의 체크 여부에 따라 #check-age 체크 여부 변경
+$('#all-check').on('change', function() {
+  $('#check-age').prop('checked', $(this).prop('checked'));
+  $('#check-provision').prop('checked', $(this).prop('checked'));
+  $('#check-use').prop('checked', $(this).prop('checked'));
+  $('#check-marketing').prop('checked', $(this).prop('checked'));
+  $('#check-sms').prop('checked', $(this).prop('checked'));
+});
+
+// 모든 항목이 check가 아닐 경우 all-check가 풀리게
+function checkboxCheck() {
+  if (
+      $('#check-age').prop('checked') &&
+      $('#check-provision').prop('checked') &&
+      $('#check-use').prop('checked') &&
+      $('#check-marketing').prop('checked') &&
+      $('#check-sms').prop('checked')
+  ) {
+      $('#all-check').prop('checked', true);
+      console.log("check");
+  } else {
+      $('#all-check').prop('checked', false);
+      console.log("check-none");
+  }
+}
+
+//------------------------------------------------------------------------
+
+/*  전체 항목이 충족되어야 회원가입하기 버튼 눌리게 */
+
+// $('#all-check').prop('checked')
+
+function areAllFieldsValid() {
+  return (
+    emailNecessaryCheck() &&
+    pwNecessaryCheck() &&
+    pwCheckNecessaryCheck() &&
+    nicknameCheck() &&
+    pwCheck() &&
+    pwDoubleCheck()
+  );
+}
+
+// 모든 필수 항목이 충족되어야 login-btn이 활성화됨
+function updateLoginButton() {
+  if (areAllFieldsValid() === false) {
+    console.log("false")
+    $('#join-btn').css('pointer-events','none');
+    $('input').css('outline','0.5px solid red');
+  } 
+}
+
+
+/* join-btn 클릭 함수 */
+function joinButtonClick() {
+  console.log('click');
+}
+
+$('.join-btn').click(joinButtonClick);
+
+
+
+
+
+
+
+
--- bin/main/static/js/join.js
+++ bin/main/static/js/join.js
@@ -1,179 +1,234 @@
-/* 필수항목 체크 함수 */
-function emailNecessaryCheck() {
-  if($('#email').val() === ''){
-    $('#email-hidden2').css('display','block');
-    $('#email').css('outline','0.5px solid red');
-    $('#email-title').css('color','#f67877');
-    return false;
-  } else {
-    $('#email-hidden2').css('display','none');
-    $('#email').css('outline','none');
-    $('#email-title').css('color','#000');
-    $('#email-btn').css('pointer-events','auto');
-    return true;
-  }
+// 조건에 맞지 않을 때
+function resetStyles(hiddenElementSelector, targetElementSelector, titleElementSelector) {
+  $(hiddenElementSelector).css('display', 'block');
+  $(targetElementSelector).css('outline', '0.5px solid red');
+  $(titleElementSelector).css('color', '#f67877');
 }
+
+// 조건에 맞을 때
+function onStyles(hiddenElementSelector, targetElementSelector, titleElementSelector) {
+  $(hiddenElementSelector).css('display', 'none');
+  $(targetElementSelector).css('outline', 'none');
+  $(titleElementSelector).css('color', '#000');
+}
+
+
+/* 이메일 함수 */
+$(function(){
+  $('#email').on('click', function() {
+    if ($(this).val() === '') {
+      resetStyles('#email-hidden2', '#email', '#email-title');
+      $('#email-btn').css('pointer-events','none');
+    } else {
+      onStyles('#email-hidden2', '#email', '#email-title');
+      $('#email-btn').css('pointer-events','auto');
+      return true;      
+    }
+  });
+  
+  $('#email').on('input', function() {
+    if ($(this).val() === '') {
+      resetStyles('#email-hidden2', '#email', '#email-title');
+      $('#email-btn').css('pointer-events','none');
+    } else {
+      onStyles('#email-hidden2', '#email', '#email-title');
+      $('#email-btn').css('pointer-events','auto');  
+      return true;
+    }
+  });
+
+
+
 
 /* email-btn 클릭 함수 */
 function emailButtonClick() {
   console.log('click');
+  return true;
 }
 
 $('#email-btn').click(emailButtonClick);
 
-function pwNecessaryCheck() {
-  if($('#password').val() === ''){
-    $('#pw-hidden2').css('display','block');
-    $('#password').css('outline','0.5px solid red');
-    return false;
-  }  else {
-    $('#pw-hidden2').css('display','none');
-    return true;
-  }
-}
 
-function pwCheckNecessaryCheck() {
-  if($('#password-check').val() === ''){
-    $('#pwCheck-hidden2').css('display','block');
-    $('#password-check').css('outline','0.5px solid red');
-    return false;
-  } else {
-    $('#pwCheck-hidden2').css('display','none');
-    return true;
-  }
-}
-
-function nicknameCheck() {
-  if($('#nickname').val() === ''){
-    $('#nickname-hidden2').css('display','block');
-    $('#nickname').css('outline','0.5px solid red');
-    $('#nickname-title').css('color','#f67877');
-    return false;
-  } else {
-    $('#nickname-hidden2').css('display','none');
-    $('#nickname').css('outline','none');
-    $('#nickname-title').css('color','#000');
-    return true;
-  }
-}
-
-//------------------------------------------------------------------------
-
-/* 유효성 체크 */
-function pwCheck() {
+/* 비밀번호 함수 */
   var pw = $("#password").val();
   var number = pw.search(/[0-9]/g);
   var english = pw.search(/[a-z]/ig);
   
-  if (pw.length < 8 || number < 0 || english < 0) {
-    $('#pw-hidden').css('display','block');
-    $('#password').css('outline','0.5px solid red');
-    $('#pw-title').css('color','#f67877');
-    return false;
-  } else {
-    $('#pw-hidden').css('display','none');
-    $('#password').css('outline','none');
-    $('#pw-title').css('color','#000');
-    return true;
-  }
-}
+  // 필수값 체크
+  $('#password').on('click', function() {
+    if ($('#password').val() === '') {
+      resetStyles('#pw-hidden2', '#password', '#pw-title');
+    } else {
+      onStyles('#pw-hidden2', '#password', '#pw-title');     
+      return true;
+    }
+  });
+  
+  $('#password').on('input', function() {
+    if ($('#password').val() === '') {
+      resetStyles('#pw-hidden2', '#password', '#pw-title');
+    } else {
+      onStyles('#pw-hidden2', '#password', '#pw-title');
+      return true;
+    }
+  });
 
 
-function pwDoubleCheck() {
-  if ($('#password').val() !== $('#password-check').val()) {
-    $('#pwCheck-hidden').css('display', 'block');
-    $('#password-check').css('outline','0.5px solid red');
-    $('#pw-check-title').css('color','#f67877');
-    return false;
-  } else {
-    $('#pwCheck-hidden').css('display', 'none');
-    $('#password-check').css('outline','none');
-    $('#pw-check-title').css('color','#000');
-    return true;
-  }
-}
+  // 유효성 검사
+  $('#password').on('input', function() {
+      var pw = $("#password").val();
+      var number = pw.search(/[0-9]/g);
+      var english = pw.search(/[a-z]/ig);
+      if (pw.length < 8 || number < 0 || english < 0) {
+      resetStyles('#pw-hidden', '#password', '#pw-title');
+    } else {
+      onStyles('#pw-hidden', '#password', '#pw-title');
+      return true;
+    }
+  });
+  
+  
+  /* 비밀번호 체크 함수 */
+    // 필수값 체크
+  $('#password-check').on('click', function() {
+    if ($('#password-check').val() === '') {
+      resetStyles('#pwCheck-hidden2', '#password-check', '#pw-check-title');
+    } else {
+      onStyles('#pwCheck-hidden2', '#password-check', '#pw-check-title');  
+      return true;   
+    }
+  });
+  
+  $('#password-check').on('input', function() {
+    if ($('#password-check').val() === '') {
+      resetStyles('#pwCheck-hidden2', '#password-check', '#pw-check-title');
+    } else {
+      onStyles('#pwCheck-hidden2', '#password-check', '#pw-check-title');  
+      return true; 
+    }
+  });
 
-//------------------------------------------------------------------------
 
-/* checkbox */
+  // 유효성 검사
+  $('#password-check').on('input', function() {
+    if ($('#password').val() !== $('#password-check').val()) {
+      resetStyles('#pwCheck-hidden', '#password-check', '#pw-check-title');
+    } else {
+      onStyles('#pwCheck-hidden', '#password-check', '#pw-check-title'); 
+      return true;
+    }
+  });
+  
+
+
+/* 닉네임 함수 */
+  $('#nickname').on('click', function() {
+    if ($(this).val() === '') {
+      resetStyles('#nickname-hidden2', '#nickname', '#nickname-title');
+    } else {
+      onStyles('#nickname-hidden2', '#nickname', '#nickname-title');
+      return true;
+    }
+  });
+  
+  $('#nickname').on('input', function() {
+    if ($(this).val() === '') {
+      resetStyles('#nickname-hidden2', '#nickname', '#nickname-title');
+    } else {
+      onStyles('#nickname-hidden2', '#nickname', '#nickname-title');
+      return true;
+    }
+  });
+
+
+
+/* 약관동의 함수 */
 // #all-check의 체크 여부에 따라 #check-age 체크 여부 변경
 $('#all-check').on('change', function() {
-  $('#check-age').prop('checked', $(this).prop('checked'));
-  $('#check-provision').prop('checked', $(this).prop('checked'));
-  $('#check-use').prop('checked', $(this).prop('checked'));
-  $('#check-marketing').prop('checked', $(this).prop('checked'));
-  $('#check-sms').prop('checked', $(this).prop('checked'));
+  $('.checkbox-btn').prop('checked', $(this).prop('checked'));
 });
 
 // 모든 항목이 check가 아닐 경우 all-check가 풀리게
-function checkboxCheck() {
+$('.checkbox-btn').on('click', function() {
+  console.log('checkbox-check');
   if (
-      $('#check-age').prop('checked') &&
-      $('#check-provision').prop('checked') &&
-      $('#check-use').prop('checked') &&
-      $('#check-marketing').prop('checked') &&
-      $('#check-sms').prop('checked')
+    $('#check-age').prop('checked') &&
+    $('#check-provision').prop('checked') &&
+    $('#check-use').prop('checked') &&
+    $('#check-marketing').prop('checked') &&
+    $('#check-sms').prop('checked')
   ) {
-      $('#all-check').prop('checked', true);
-      console.log("check");
+    $('#all-check').prop('checked', true);
+    console.log('1');
   } else {
-      $('#all-check').prop('checked', false);
-      console.log("check-none");
+    console.log('2');
+    $('#all-check').prop('checked', false);
   }
-}
-
-//------------------------------------------------------------------------
-
-/*  전체 항목이 충족되어야 회원가입하기 버튼 눌리게 */
-
-// $('#all-check').prop('checked')
-
-function areAllFieldsValid() {
-  return (
-    emailNecessaryCheck() &&
-    pwNecessaryCheck() &&
-    pwCheckNecessaryCheck() &&
-    nicknameCheck() &&
-    pwCheck() &&
-    pwDoubleCheck()
-  );
-}
-
-// 모든 필수 항목이 충족되어야 login-btn이 활성화됨
-function updateLoginButton() {
-  if (areAllFieldsValid() === false) {
-    console.log("false")
-    $('#join-btn').css('pointer-events','none');
-    $('input').css('outline','0.5px solid red');
-  } 
-}
-
-// 필수 항목이 변경될 때마다 업데이트
-$('#email, #password, #password-check, #nickname').on('input', updateLoginButton);
-
-// 체크박스 상태 변경 시 업데이트
-$(':checkbox').on('change', function() {
-  checkboxCheck();
-  updateLoginButton();
 });
 
 
-/* join-btn 클릭 함수 */
-function joinButtonClick() {
-  console.log('click');
-}
-
-$('.join-btn').click(joinButtonClick);
 
 
+/* 회원가입하기 버튼 함수 */
+$('#join-btn').on('click', function() {
+  console.log("이메일",checkEmail());
+  console.log("비밀번호",checkPassword());
+  console.log("비밀번호 체크",checkPasswordMatch());
+  console.log("닉네임",checkNickname());
+  console.log("전체동의",checkAgreement());
+  console.log("로봇",checkRobot());
+    // 각 함수의 반환 값이 모두 true인지 확인
+    if (
+      checkEmail() &&
+      checkPassword() &&
+      checkPasswordMatch() &&
+      checkNickname() &&
+      checkAgreement() &&
+      checkRobot()
+    ) {
+      alert("회원가입 성공");
+      console.log("회원가입 성공");
+    } else {
+      alert("회원가입 실패");
+      console.log('회원가입 실패');
+    }
+  });
+
+  // 각 함수의 반환 값이 true 또는 false인지 확인하는 함수들
+  function checkEmail() {
+    return $('#email').val() !== ''; 
+  }
+
+  function checkPassword() {
+    return $('#password').val() !== ''; 
+  }
+
+  function checkPasswordMatch() {
+    return $('#password-check').val() !== ''; 
+  }
+
+  function checkNickname() {
+    return $('#nickname').val() !== ''; 
+  }
+
+  function checkAgreement() {
+    if(
+      $('#check-age').is(':checked') &&
+      $('#check-provision').is(':checked') &&
+      $('#check-use').is(':checked')
+    ){
+      return true;
+    } else {
+      return false;
+    }  
+
+  }
+
+  function checkRobot() {
+    return $('#robot-check').is(':checked');
+
+  }
 
 
 
-
-
-
-
-
-
-
-
+});
