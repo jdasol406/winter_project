2f511aa3ed6806c9561805309c14eea1195e871a
jdasol406 <jdasol406@gmail.com>
2023-12-09 23:45:31 +0900

    20231209 duplication js

--- bin/main/static/js/article.js
+++ bin/main/static/js/article.js
@@ -30,40 +30,61 @@ $('#site-cancel').on('click',function(){
   $('.popup').css('display','none');
 }); 
 
+
 // 스크립트로 html만들고 마지막요소에 붙이는 함수
 $('#site-check').on('click', function() {
+  
   var inputValue = $('#site-name').val();
+  
   console.log("test : "+inputValue);
+  
     if(inputValue==="daum"){
       console.log("daum");
-      var imageElement = $('<img>', {
-        src: '/img/article/daum-favicon.png',
-        style: 'width: 22px; border-radius: 5px; margin: 8px auto'
-      });
-      const dynamicDiv = $('<div>').append(imageElement);
+      
+      if($('#daum').length === 0){
+        var imageElement = $('<img>', {
+          id: 'daum',
+          src: '/img/article/daum-favicon.png',
+          style: 'width: 22px; border-radius: 5px; margin: 8px auto'
+        });
+        const dynamicDiv = $('<div>').append(imageElement);
+  
+        $('#adddiv').append(dynamicDiv);
+      } else {
+        alert('이미 존재하는 항목입니다.');
+      }
+   } else if (inputValue==="apple"){
+      console.log("apple");
+      
+      if($('#apple').length === 0){
+        var imageElement = $('<img>', {
+          id: 'apple',
+          src: '/img/article/apple-favicon.jpeg',
+          style: 'width: 22px; border-radius: 5px; margin: 8px auto'
+        });
+        const dynamicDiv = $('<div>').append(imageElement);
+  
+        $('#adddiv').append(dynamicDiv);
+      } else {
+        alert('이미 존재하는 항목입니다.');
+      }
+   } else if(inputValue==="papago"){
+      console.log("papago");
+      
+      if($('#papago').length === 0){
+        var imageElement = $('<img>', {
+          id: 'papago',
+          src: '/img/article/papago-favicon.png',
+          style: 'width: 22px; border-radius: 5px; margin: 8px auto'
+        });
+        const dynamicDiv = $('<div>').append(imageElement);
+  
+        $('#adddiv').append(dynamicDiv);
+      } else {
+        alert('이미 존재하는 항목입니다.');
+      }
+   }
+   
+});
 
-      $('#adddiv').append(dynamicDiv);
-    } else if(inputValue==="apple"){
-       console.log("apple");
-      var imageElement = $('<img>', {
-        src: '/img/article/apple-favicon.jpeg',
-        style: 'width: 22px; border-radius: 5px; margin: 8px auto'
-      });
-      const dynamicDiv = $('<div>').append(imageElement);
-
-      $('#adddiv').append(dynamicDiv);
-    } else if(inputValue==="papago"){
-       console.log("papago");
-      var imageElement = $('<img>', {
-        src: '/img/article/papago-favicon.png',
-        style: 'width: 22px; border-radius: 5px; margin: 8px auto'
-      });
-      const dynamicDiv = $('<div>').append(imageElement);
-
-      $('#adddiv').append(dynamicDiv);
-    }
-
-
-
-    });
 
