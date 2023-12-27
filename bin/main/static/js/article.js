43cefc480058ce9fc0d91e708f75609924372220
jdasol406 <jdasol406@gmail.com>
2023-12-28 00:13:08 +0900

    20231228 script simplify

--- bin/main/static/js/article.js
+++ bin/main/static/js/article.js
@@ -21,7 +21,9 @@ $('.rolling-rank').mouseout (function() {
 
 /* container */
 
+/* fixed menu script */
 $('#plus').on('click',function(){
+  $('.popup input[type="text"]').val('');
   $('.popup').find('#site-check').show();
   $('.popup').css('display','block');
 }); 
@@ -32,71 +34,64 @@ $('#site-cancel').on('click',function(){
   $('.popup input[type="text"]').val('');
 }); 
 
+var sharpIcon = ['#daum','#apple','#papago'];
 
+for(let i=0; i<sharpIcon.length; i++){
+  console.log(sharpIcon[i]);
+}
+
+var icon = ['daum','apple','papago'];
+
+for(let i=0; i<icon.length; i++){
+  console.log(icon[i]);
+}
+
+// 새로운 아이콘을 생성하는 함수
 function siteCheckHandler() {
   var inputValue = $('#site-name').val();
-  
-    if(inputValue==="daum"){
-            
-      if($('#daum').length === 0){
-        var imageElement = $('<img>', {
-          id: 'daum',
-          src: '/img/article/daum-favicon.png',
-          style: 'width: 22px; border-radius: 5px; margin: 8px auto'
-        });
-        const dynamicDiv = $('<div>').append(imageElement);
-  
-        $('#adddiv').prepend(dynamicDiv);
-      } else {
-        alert('이미 존재하는 항목입니다.');
-      }
-   } else if (inputValue==="apple"){
-      
-      if($('#apple').length === 0){
-        var imageElement = $('<img>', {
-          id: 'apple',
-          src: '/img/article/apple-favicon.png',
-          style: 'width: 20px; height: 20px; border-radius: 5px; margin: 8px auto'
-        });
-        const dynamicDiv = $('<div>').append(imageElement);
-  
-        $('#adddiv').prepend(dynamicDiv);
-      } else {
-        alert('이미 존재하는 항목입니다.');
-      }
-   } else if(inputValue==="papago"){
-      
-      if($('#papago').length === 0){
-        var imageElement = $('<img>', {
-          id: 'papago',
-          src: '/img/article/papago-favicon.png',
-          style: 'width: 22px; border-radius: 5px; margin: 8px auto'
-        });
-        const dynamicDiv = $('<div>').append(imageElement);
-  
-        $('#adddiv').prepend(dynamicDiv);
-      } else {
-        alert('이미 존재하는 항목입니다.');
-      }
-   } else {
-     alert("생성할 수 없는 사이트입니다.");
-   }
+  //indexOf() 함수로 icon배열에서 inputValue의 인덱스를 찾음
+  var iconIndex = icon.indexOf(inputValue);
+
+  if (iconIndex !== -1) { // 배열에서 찾는 요소가 없으면 -1을 반환함
+    var sharpIconId = sharpIcon[iconIndex];
+
+    if ($(sharpIconId).length === 0) {
+      var imageElement = $('<img>', {
+        id: icon[iconIndex],
+        src: '/img/article/' + icon[iconIndex] + '-favicon.png',
+        style: 'width: 22px; border-radius: 5px; margin: 8px auto'
+      });
+      const dynamicDiv = $('<div>').append(imageElement);
+
+      $('#adddiv').prepend(dynamicDiv);
+    } else {
+      alert('이미 존재하는 항목입니다.');
+    }
+  } else {
+    alert('생성할 수 없는 사이트입니다.');
+  }
+ 
    $('.popup').css('display','none');
    $('.popup input[type="text"]').val('');
 }
 
+// 생성함수 호출
 $('#site-check').on('click', siteCheckHandler);
 
+
+// 아이콘 이미지, 아이디 변경 함수
 function replaceImage(src, newId) {
   $('.imgx').attr('src', src).attr('id', newId);
   $('.imgx').removeClass('imgx');
 }
 
+// 원래 이미지로 돌리는 함수
 function imgback(src, newId) {
   $('.imgx').attr('src', src).attr('id', newId);
   $('.imgx').removeClass('imgx');
 }
 
+// x이미지 눌렀을 때 아이콘 없애는 함수
 function ximg(e){
   $(e).attr('src', '/img/article/x-icon.png');
   $(e).addClass('imgx'); 
@@ -107,100 +102,27 @@ function ximg(e){
   });
 }
 
-$(document).on('contextmenu', '#daum', function(event) {
+// 수정 팝업 열리는 함수
+function updatePopupOpen(event) {
   var elementId = event.target.id;
-  ximg(this);
-
+  
   $('.popup input#site-name').val(elementId);  
   $('.popup').find('#site-check').hide();
   $('.popup').css('display','block');
-  
-  
-  // 이전 핸들러 제거
-  $('#site-cancel').off('click').on('click', function() {
-    imgback('/img/article/'+ elementId + '-favicon.png', elementId);
-  });
-  
-  $('#site-cancel').on('click', function() {
-    imgback('/img/article/'+ elementId + '-favicon.png', elementId);
-    $('.popup').css('display','none');
-  });
-  
-  // 이전 핸들러 제거
-  $('#site-update').off('click');
-  
-  $('#site-update').on('click', function() {
-    var newText = $('.popup input').val();
-    
-     if($('#'+newText).length === 0) {
-      replaceImage('/img/article/' + newText + '-favicon.png',newText);
-      } else {
-        alert('이미 존재하는 항목입니다.');
-        imgback('/img/article/'+ elementId + '-favicon.png', elementId);
-      }
-      
-    $('.popup').css('display','none');
-    $('.popup input[type="text"]').val('');
-  });
-});
+}
 
-$(document).on('contextmenu', '#apple', function(event) {
-  var elementId = event.target.id;
-  ximg(this);
-  
-  $('.popup input#site-name').val(elementId);
-  $('.popup').find('#site-check').hide();
-  $('.popup').css('display','block');
-  
-  $('#site-cancel').off('click').on('click', function() {
-    imgback('/img/article/'+ elementId + '-favicon.png', elementId);
-    $('.popup').css('display','none');
-  });
-  
-
-  $('#site-cancel').on('click', function() {
-    imgback('/img/article/'+ elementId + '-favicon.png', elementId);
-    $('.popup').css('display','none');
-  });
-  
-  $('#site-update').off('click');
-  
-  $('#site-update').on('click', function() {
-    var newText = $('.popup input').val();
-    
-    if($('#'+newText).length === 0) {
-      replaceImage('/img/article/' + newText + '-favicon.png',newText);
-    } else {
-      alert('이미 존재하는 항목입니다.');
-      imgback('/img/article/'+ elementId + '-favicon.png', elementId);
-    }
-    $('.popup').css('display','none');
-    $('.popup input[type="text"]').val('');
-  });
-});
-
-$(document).on('contextmenu', '#papago', function(event) {
+// 팝업 취소 버튼 눌렀을 때 함수
+function cancelBtn(event){ //못씀
   var elementId = event.target.id;
   
-  ximg(this);
-  
-  $('.popup input#site-name').val(elementId);
-  $('.popup').find('#site-check').hide();
-  $('.popup').css('display','block');
-  
-  $('#site-cancel').off('click').on('click', function() {
-    imgback('/img/article/'+ elementId + '-favicon.png', elementId);
-  });
-  
-  $('#site-cancel').on('click', function() {
-    imgback('/img/article/'+ elementId + '-favicon.png', elementId);
+  imgback('/img/article/'+ elementId + '-favicon.png', elementId);
     $('.popup').css('display','none');
-  });
-  
-  $('#site-update').off('click');
-  
-  $('#site-update').on('click', function() {
-    var newText = $('.popup input').val();
+}
+
+// 팝업 업데이트 버튼 눌렀을 때 함수
+function updateBtn(event){
+  var elementId = event.target.id;
+  var newText = $('.popup input').val();
 
     if($('#'+newText).length === 0) {
       replaceImage('/img/article/' + newText + '-favicon.png',newText);
@@ -210,7 +132,34 @@ $(document).on('contextmenu', '#papago', function(event) {
     }
     $('.popup').css('display','none');
     $('.popup input[type="text"]').val('');
-  });
-});
+}
 
+/* ------------------------------------------------------ */
+
+// 아이콘 수정부분 스크립트
+for (var i = 0; i < icon.length; i++) {
+  $(document).on('contextmenu', '#' + icon[i], function (event) {
+    var elementId = event.target.id;
+    
+    ximg(this);
+
+    // 생성버튼이 사라진 수정팝업 오픈
+    updatePopupOpen(event);
+
+    $('#site-cancel').off('click').on('click', function () {
+      imgback('/img/article/' + elementId + '-favicon.png', elementId);
+      $('.popup').css('display', 'none');
+    });
+
+  $('#site-cancel').on('click', function () {
+    cancelBtn(event);
+  });
+
+    $('#site-update').off('click').on('click', function () {
+      updateBtn(event);
+    });
+  });
+}
+
+/* fixed menu script end */
 
