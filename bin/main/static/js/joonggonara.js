fd450bf1272f5c1057329977a092380992c2bfb6
jdasol406 <jdasol406@gmail.com>
2024-01-10 00:24:01 +0900

    20240110 writeBoard

--- bin/main/static/js/joonggonara.js
+++ bin/main/static/js/joonggonara.js
@@ -1,3 +1,6 @@
+/*
+페이지 이동할 때 코드
+
 // writeBoard 페이지에서 table을 새로 만들어서 로컬스토리지에 저장하는 코드
 $(document).ready(function() {
   console.log("test");
@@ -44,4 +47,63 @@ function clearTable() {
     $("#tableContainer").find("tbody").empty();
     localStorage.removeItem('tableData');
 }
+*/
+
+// 글쓰기 버튼
+function writeBoard() {
+  $("#input-title").val('');
+  $("#input-price").val('');
+  $("#input-category").val('');
+     
+  $('.writeBoard').css('display','block');
+}
+
+// 등록 버튼
+$(function() {
+  console.log("test");
+  $("#registration-btn").on("click", function() {
+      console.log("registeration");
+      
+      $('.writeBoard').css('display','none');
+      
+      var title = $("#input-title").val();
+      var price = $("#input-price").val();
+      var category = $("#input-category").val();
+
+      var newRow = '<tr id="' + title + '" style="text-align: center;">';
+      newRow += '<td style="text-align: center;"></td>';
+      newRow += '<td style="text-align: center;">' + title + '</td>';
+      newRow += '<td style="text-align: center;">작성자</td>';
+      newRow += '<td style="text-align: center;">작성일</td>';
+      newRow += '<td style="text-align: center;">조회</td>';
+   
+     $("#tableContainer").find("tbody").append(newRow);
+     
+     $("#input-title").val('');
+     $("#input-price").val('');
+     $("#input-category").val('');
+  });
+});
+
+// table 클릭 시 writeBoard open
+$(document).on("click", "#tableContainer tbody tr", function() {
+  var clickedRow = $(this);
+      
+  var title = clickedRow.find("td:eq(1)").text(); 
+
+  $("#input-title").val(title);
+
+  $('#update-btn').css('display', 'block');
+  $('.writeBoard').css('display', 'block');
+});
+
+// 취소 버튼
+$("#cancel-btn").on("click", function() {
+  $('.writeBoard').css('display','none');
+});
+
+
+
+
+
  
