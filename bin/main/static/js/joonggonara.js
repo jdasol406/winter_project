a2832ca5f89b7986fdfa2c910bf810c4156863b0
jdasol406 <jdasol406@gmail.com>
2024-01-18 00:01:04 +0900

    20240118 post js

--- bin/main/static/js/joonggonara.js
+++ bin/main/static/js/joonggonara.js
@@ -1,3 +1,102 @@
+var postArray = [];
+
+// 글쓰기 버튼
+function writeBoard() {
+  $("#input-title").val('');
+  $("#input-price").val('');
+  $("#input-category").val('');
+     
+  $('.writeBoard').css('display','block');
+}
+
+// 등록 버튼
+$(function() {
+  console.log("test");
+  $("#registration-btn").on("click", function() {
+      console.log("registeration");
+      
+      $('.writeBoard').css('display','none');
+      
+      var title = $("#input-title").val();
+      var price = $("#input-price").val();
+      var category = $("#input-category").val();
+      
+      var rowCount = $("#tableContainer tbody tr").length + 1;
+      
+
+      var newRow = '<tr key="' + rowCount + '" style="text-align: center;">';
+      newRow += '<td style="text-align: center;">' + rowCount + '</td>';
+      newRow += '<td style="text-align: center;">' + title + '</td>';
+      newRow += '<td style="text-align: center;">작성자</td>';
+      newRow += '<td style="text-align: center;">작성일</td>';
+      newRow += '<td style="text-align: center;">조회</td>';
+      
+      
+     $("#tableContainer").find("tbody").prepend(newRow);
+     
+     // 행 정보를 하나의 배열로 묶어서 배열에 추가
+     var obj = {
+       rowCount: rowCount,
+       title: title,
+       price: price,
+       category: category
+     }
+     postArray.push(obj);
+      
+     $("#input-title").val('');
+     $("#input-price").val('');
+     $("#input-category").val('');
+     
+  });
+});
+
+
+$("#tableContainer tbody").on("click", "tr", function() {
+    var clickedRowId = $(this).attr("key");
+
+    var clickedObject = postArray.find(function (obj) {
+        return obj.rowCount == clickedRowId;
+     });
+
+    console.log(clickedObject);
+    console.log(clickedObject.title);
+    console.log(clickedObject.price);
+    console.log(clickedObject.category);
+    
+    $("#post-title").val(clickedObject.title);
+    $("#post-price").val(clickedObject.price);
+    $("#post-category").val(clickedObject.category);
+    
+    
+    $('.post').css('display', 'block');
+    
+});
+
+// 취소 버튼
+$("#cancel-btn").on("click", function() {
+  $('.writeBoard').css('display','none');
+});
+
+// 목록 버튼
+$("#list").on("click", function() {
+  $('.post').css('display','none');
+});
+
+
+
+
+// table 클릭 시 writeBoard open
+//$(document).on("click", "#tableContainer tbody tr", function() {
+//  var clickedRow = $(this);
+//      
+//  var postTitle = clickedRow.find("td:eq(1)").text(); 
+//
+//  $("#post-title").val(postTitle);
+//  
+//
+//  $('.post').css('display', 'block');
+//});
+
 /*
 페이지 이동할 때 코드
 
@@ -48,83 +147,6 @@ function clearTable() {
     localStorage.removeItem('tableData');
 }
 */
-
-// 글쓰기 버튼
-function writeBoard() {
-  $("#input-title").val('');
-  $("#input-price").val('');
-  $("#input-category").val('');
-     
-  $('.writeBoard').css('display','block');
-}
-
-// 등록 버튼
-$(function() {
-  console.log("test");
-  $("#registration-btn").on("click", function() {
-      console.log("registeration");
-      
-      $('.writeBoard').css('display','none');
-      
-      var title = $("#input-title").val();
-      var price = $("#input-price").val();
-      var category = $("#input-category").val();
-      
-
-      var newRow = '<tr id="' + title + '" style="text-align: center;">';
-      newRow += '<td style="text-align: center;"></td>';
-      newRow += '<td style="text-align: center;">' + title + '</td>';
-      newRow += '<td style="text-align: center;">작성자</td>';
-      newRow += '<td style="text-align: center;">작성일</td>';
-      newRow += '<td style="text-align: center;">조회</td>';
-   
-     $("#tableContainer").find("tbody").append(newRow);
-     
-      
-      newInputs(title);
-
-     
-     $("#input-title").val('');
-     $("#input-price").val('');
-     $("#input-category").val('');
-  });
-});
-
-//input 생성 //아니야 input의 value값을 가져와야해 수정해야해 여기 ㅠㅠㅠㅠㅠㅠㅠㅠ
-function newInputs(title){
-  var newInputsHTML = `
-    <input class="${title}" id="post-title" style="margin-top: 10px;" type="text" placeholder="상품명(제목)">
-    <input class="${title}" id="post-price" type="text" placeholder="가격을 입력하세요">
-    <input class="${title}" id="post-category" type="text" placeholder="상품 카테고리">
-  `;
-  
-  // 생성된 HTML 문자열을 post-input 클래스를 가진 요소에 추가
-  document.querySelector('.post-input').innerHTML = newInputsHTML;
-}
-
-// table 클릭 시 writeBoard open
-$(document).on("click", "#tableContainer tbody tr", function() {
-  var clickedRow = $(this);
-      
-  var postTitle = clickedRow.find("td:eq(1)").text(); 
-
-  $("#post-title").val(postTitle);
-  
-
-  $('.post').css('display', 'block');
-});
-
-// 취소 버튼
-$("#cancel-btn").on("click", function() {
-  $('.writeBoard').css('display','none');
-});
-
-// 목록 버튼
-$("#list").on("click", function() {
-  $('.post').css('display','none');
-});
-
-
 
 
 
