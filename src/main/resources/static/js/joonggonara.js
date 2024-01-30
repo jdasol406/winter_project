50bf217d830b33879a247c8da5f51c0fb2838b0d
jdasol406 <jdasol406@gmail.com>
2024-01-31 00:00:47 +0900

    20240130 script

--- src/main/resources/static/js/joonggonara.js
+++ src/main/resources/static/js/joonggonara.js
@@ -1,5 +1,7 @@
+/* global variation */
 var postArray = [];
 
+/* function */
 //input 비우는 함수
 function resetInput(){
   $("#input-title").val('');
@@ -14,41 +16,54 @@ function writeBoard() {
   $('.writeBoard').css('display','block');
 }
 
-
-// 등록 버튼
-$("#registration-btn").on("click", function() {
+// table만드는 함수
+function createTable(array) {
+  $("#tableContainer tbody").empty();
+  
+  var tbody = $("#tableContainer").find("tbody");
+  
+  // 배열 내의 각 객체에 대해 행을 생성
+  for (var i = 0; i < array.length; i++) {
+    var row = array[i];
     
-    $('.writeBoard').css('display','none');
-    
-    var title = $("#input-title").val();
-    var price = $("#input-price").val();
-    var category = $("#input-category").val();
-    
-    var rowCount = $("#tableContainer tbody tr").length + 1;
-    
-    // 행 정보를 하나의 배열로 묶어서 배열에 추가
-   var obj = {
-     rowCount: rowCount,
-     title: title,
-     price: price,
-     category: category
-   }
-   postArray.push(obj);
-    
-   resetInput();
-
-    // 이렇게 테이블을 만들지 말고 배열 안의 값을 가져와서 ...
-    var newRow = '<tr key="' + rowCount + '" style="text-align: center;">';
-    newRow += '<td style="text-align: center;">' + rowCount + '</td>';
-    newRow += '<td style="text-align: center;">' + title + '</td>';
+    var newRow = '<tr key="' + row.rowCount + '" style="text-align: center;">';
+    newRow += '<td style="text-align: center;">' + row.rowCount + '</td>';
+    newRow += '<td style="text-align: center;">' + row.title + '</td>';
     newRow += '<td style="text-align: center;">작성자</td>';
     newRow += '<td style="text-align: center;">작성일</td>';
     newRow += '<td style="text-align: center;">조회</td>';
     
+    //추가를 한번에 해야함
+    tbody.prepend(newRow);
+  }
+}
+
+// inputReadOnly 함수
+function inputReadOnly(bool){
+  $("#post-title").prop('readOnly', bool);
+  $("#post-price").prop('readOnly', bool);
+  $("#post-category").prop('readOnly', bool);
+}
+
+// 등록 버튼
+$("#registration-btn").on("click", function() {
+  $(".writeBoard").css("display","none");
     
-   $("#tableContainer").find("tbody").prepend(newRow);
-   
-   
+  var title = $("#input-title").val();
+  var price = $("#input-price").val();
+  var category = $("#input-category").val(); 
+  var rowCount = $("#tableContainer tbody tr").length + 1;
+
+  var obj = { // 행 정보를 하나의 배열로 묶어서 배열에 추가
+    rowCount,
+    title,
+    price,
+    category
+  };
+  postArray.push(obj);
+
+  resetInput();
+  createTable(postArray); 
 });
 
 // 이벤트 위임으로 동적으로 추가되는 tr요소들에 대한 이벤트를 처리할 수 있도록 해줌
@@ -56,37 +71,51 @@ $("#tableContainer tbody").on("click", "tr", function() {
   var clickedRowId = $(this).attr("key");
 
   var clickedObject = postArray.find(function (obj) {
-      return obj.rowCount == clickedRowId;
-   });
-   
+    return obj.rowCount == clickedRowId;
+  });
    
   $("#post-title").val(clickedObject.title);
   $("#post-price").val(clickedObject.price);
   $("#post-category").val(clickedObject.category);
+  $("#hidden-key").val(clickedRowId);
   
-  $("#post-title").prop('readOnly', true);
-  $("#post-price").prop('readOnly', true);
-  $("#post-category").prop('readOnly', true);
+  inputReadOnly(true);
 
   $('#update-btn').css('display','none');
-  
   $('.post').css('display', 'block');
-  console.log("clickedRowId "+clickedRowId);
-  console.log(JSON.stringify(postArray, null, 2));
-  
-  // 삭제 버튼
-  $("#delete-btn").on("click", function() {
-    console.log("clickedRowId "+clickedRowId);
-    var indexToRemove = postArray.findIndex(obj => obj.rowCount === parseInt(clickedRowId));
-    
-    postArray.splice(indexToRemove, 1);  // indexToRemove에서 1개의 요소를 삭제
-    
-    $("#tableContainer tbody tr[key='" + clickedRowId + "']").remove();
-    
-    $('.post').css('display','none');
 
-  });
-    
+  
+});
+
+// 삭제 버튼 여러번 바인딩...?ㅡㅡ 되는걸 막으려고 .off("click")을 넣음
+$("#delete-btn").on("click", function() {
+  console.log("clickedRowId del "+clickedRowId);
+  var indexToRemove = postArray.findIndex(obj => obj.rowCount === parseInt(clickedRowId));
+  
+  postArray.splice(indexToRemove, 1);  // indexToRemove에서 1개의 요소를 삭제
+  
+  //$("#tableContainer tbody tr[key='" + clickedRowId + "']").remove();
+  createTable(postArray);
+  
+  $('.post').css('display','none');
+
+});
+  
+  // 수정 버튼
+$("#update-btn").on("click", function() {
+  var title = $("#post-title").val();
+  var price = $("#post-price").val();
+  var category = $("#post-category").val();
+  
+  // 입력된 input value로 배열 값을 바꿔버림
+  postArray[clickedRowId-1].title = title;
+  postArray[clickedRowId-1].price = price;
+  postArray[clickedRowId-1].category = category;
+  
+  createTable(postArray);
+
+  $('.post').css('display','none');
+  
 });
 
 // 취소 버튼
@@ -103,35 +132,11 @@ $("#list").on("click", function() {
 $("#update-go").on("click", function() {
   $('#update-btn').css('display','block');
   $('.comment').css('display','none');
+  $('#update-go').css('display','none');
   
-  $("#post-title").prop('readOnly', false);
-  $("#post-price").prop('readOnly', false);
-  $("#post-category").prop('readOnly', false);
+  inputReadOnly(false);
   
 })
-
-// 수정 버튼
-$("#update-btn").on("click", function() {
-  var title = $("#post-title").val();
-  var price = $("#post-price").val();
-  var category = $("#post-category").val();
-  
-  console.log(title);
-  
-  // 입력된 input value로 배열 값을 바꿔버림
-  postArray[selectedRow-1].title = title;
-  postArray[selectedRow-1].price = price;
-  postArray[selectedRow-1].category = category;
-  
-  var selector = "#tableContainer tbody tr[key='" + selectedRow + "'] td:eq(1)";
-  
-  // 선택자를 사용하여 해당 td의 내용 업데이트
-  $(selector).html(title);
-
-  $('.post').css('display','none');
-  
-});
-
 
 
 
